f91275efb1f071514f1c02109c0d1d419d0d4e3a
Thiago Lima <lietlaw364@gmail.com>
2022-06-28 12:43:03 -0300

    add navItems

--- src/components/layout/AppNavbar.jsx
+++ src/components/layout/AppNavbar.jsx
@@ -1,25 +1,43 @@
 import { FaGithub } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const AppNavbar = ({ title }) => {
+  const navItems = [
+    {
+      name: "Home",
+      path: "/",
+    },
+    {
+      name: "About",
+      path: "/about",
+    },
+  ];
+
   return (
     <nav className="navbar mb-12 shadow-lg bg-neutral text-neutral-content">
       <div className="container mx-auto">
         <div className="flex-none px-2 mx-2">
           <FaGithub className="inline pr-2 text-3xl" />
-          <Link to="/" className="text-lg font-bold align-middle">
+          <NavLink to="/" className="text-lg font-bold align-middle">
             {title}
-          </Link>
+          </NavLink>
         </div>
 
         <div className="flex-1 px-2 mx-2">
           <div className="flex justify-end">
-            <Link to="/" className="btn btn-ghost btn-sm rounded-btn">
-              Home
-            </Link>
-            <Link to="/about" className="btn btn-ghost btn-sm rounded-btn">
-              About
-            </Link>
+            {navItems.map((item, index) => (
+              <NavLink
+                key={index}
+                to={item.path}
+                className={({ isActive }) =>
+                  (isActive
+                    ? "btn-primary btn btn-md mx-1 btn-rounded"
+                    : "btn-ghost btn btn-md mx-1 btn-rounded")
+                }
+              >
+                {item.name}
+              </NavLink>
+            ))}
           </div>
         </div>
       </div>
