91764febe9120fbe7fd4d2f35ae54bb020fe1a67
Thiago Lima <lietlaw364@gmail.com>
2022-07-02 17:39:56 -0300

    add user item component

---
+++ src/components/users/UserItem.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+const UserItem = ({ user: {login, avatar_url} }) => {
+  return (
+    <div className="flex items-center space-x-3">
+    <div className="avatar">
+      <div className="mask mask-squircle w-12 h-12">
+        <img src={avatar_url} alt="Avatar" />
+      </div>
+    </div>
+    <div>
+      <div className="font-bold">{login}</div>
+      <Link className="text-sm text-primary" to={`/users/${login}`}>Visit profile</Link>
+    </div>
+  </div>
+  );
+};
+
+export default UserItem;
--- src/components/users/UsersResult.jsx
+++ src/components/users/UsersResult.jsx
@@ -1,13 +1,14 @@
-import { useEffect, useState } from 'react'
-import BeatLoader from 'react-spinners/BeatLoader'
+import { useEffect, useState } from "react";
+import BeatLoader from "react-spinners/BeatLoader";
+import UserItem from "./UserItem";
 
 const UsersResult = () => {
-  const [users, setUsers] = useState([])
-  const [loading, setLoading] = useState(true)
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetchUsers()
-  }, [])
+    fetchUsers();
+  }, []);
 
   const fetchUsers = async () => {
     const response = await (
@@ -16,45 +17,30 @@ const UsersResult = () => {
           Authorization: `token ${import.meta.env.VITE_REACT_APP_GITHUB_TOKEN}`,
         },
       })
-    ).json()
+    ).json();
 
-    setUsers(response)
-    setLoading(false)
-  }
+    setUsers(response);
+    setLoading(false);
+  };
 
   if (!loading) {
     return (
       <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
-        {users.map((user, index) => (
-          <div
-            key={index}
-            className="flex-1 w-64 card card-compact bg-base-100 m-2 shadow-xl"
-          >
-            <figure>
-              <img src={user.avatar_url} alt="Shoes" />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">@{user.login}</h2>
-              {/* <p>If a dog chews shoes whose shoes does he choose?</p> */}
-              <div className="card-actions justify-end">
-                <button className="btn btn-primary">
-                  <a href={user.html_url} target="_blank">
-                    Visit
-                  </a>
-                </button>
-              </div>
-            </div>
-          </div>
+        {users.map((user) => (
+          <UserItem
+            key={user.id}
+            user={user}
+          />
         ))}
       </div>
-    )
+    );
   }
-  
+
   return (
     <div className="flex justify-center">
       <BeatLoader color="#FF7AC6" loading={loading} size={40} />
     </div>
-  )
-}
+  );
+};
 
-export default UsersResult
+export default UsersResult;
