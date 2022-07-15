e4cee50d8b3f7f7666be2fbb015ccc4270d2ae24
Thiago Lima <lietlaw364@gmail.com>
2022-07-15 13:08:34 -0300

    add visit repository

--- src/components/users/RepoItem.jsx
+++ src/components/users/RepoItem.jsx
@@ -1,19 +1,25 @@
-const RepoItem = ({repository}) => {
+const RepoItem = ({ repository }) => {
   return (
     <div className="card my-2  bg-base-100 shadow-2xl">
-    <div className="card-body">
-      <h2 className="card-title">
-        {repository.name}
-        {repository.language && (
-          <div className="badge badge-primary">{repository.language}</div>
-        )}
-      </h2>
-      <p>{repository.description}</p>
-      <div className="card-actions justify-end">
-        <a className="btn btn-outline btn-sm">Visit Repo</a>
+      <div className="card-body">
+        <h2 className="card-title">
+          {repository.name}
+          {repository.language && (
+            <div className="badge badge-primary">{repository.language}</div>
+          )}
+        </h2>
+        <p>{repository.description}</p>
+        <div className="card-actions justify-end">
+          <a
+            className="btn btn-outline btn-sm"
+            href={repository.html_url}
+            target="_blank"
+          >
+            Visit Repo
+          </a>
+        </div>
       </div>
     </div>
-  </div>
   )
 }
 
